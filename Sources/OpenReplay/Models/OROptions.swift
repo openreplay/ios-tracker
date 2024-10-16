9b35ba46b4059cd93b8252075264c25b4366f99d
Valerio <listamaster@gmail.com>
2024-10-16 16:44:23 +0200

    add minScreenCount options

--- Sources/OpenReplay/Models/OROptions.swift
+++ Sources/OpenReplay/Models/OROptions.swift
@@ -12,19 +12,21 @@ open class OROptions: NSObject {
     let performances: Bool
     let logs: Bool
     let screen: Bool
+    let minScreenCount: Int
     let wifiOnly: Bool
     let debugLogs: Bool
     let debugImages: Bool
     
-    public static let defaults = OROptions(crashes: true, analytics: true, performances: true, logs: true, screen: true, wifiOnly: true, debugLogs: false, debugImages: false)
-    public static let defaultDebug = OROptions(crashes: true, analytics: true, performances: true, logs: true, screen: true, wifiOnly: true, debugLogs: true, debugImages: false)
+    public static let defaults = OROptions(crashes: true, analytics: true, performances: true, logs: true, screen: true, minScreenCount: 20, wifiOnly: true, debugLogs: false, debugImages: false)
+    public static let defaultDebug = OROptions(crashes: true, analytics: true, performances: true, logs: true, screen: true, minScreenCount: 20, wifiOnly: true, debugLogs: true, debugImages: false)
 
-    @objc public init(crashes: Bool, analytics: Bool, performances: Bool, logs: Bool, screen: Bool, wifiOnly: Bool, debugLogs: Bool, debugImages: Bool) {
+    @objc public init(crashes: Bool = true, analytics: Bool = true, performances: Bool = true, logs: Bool = true, screen: Bool = true, minScreenCount: Int = 20, wifiOnly: Bool = true, debugLogs: Bool = false, debugImages: Bool = false) {
         self.crashes = crashes
         self.analytics = analytics
         self.performances = performances
         self.logs = logs
         self.screen = screen
+        self.minScreenCount = minScreenCount
         self.wifiOnly = wifiOnly
         self.debugLogs = debugLogs
         self.debugImages = debugImages
